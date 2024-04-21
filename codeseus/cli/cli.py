e1bf120d88b5164c100bc5a959b89897830f8b4b
kugesan1105 <kugesan.sivasothynathan@jaseci.org>
2024-04-22 03:10:40 +0530

    added multiselection and select all options

--- codeseus/cli/cli.py
+++ codeseus/cli/cli.py
@@ -36,15 +36,33 @@ def main() -> None:
     if action:
         current_dir = os.getcwd()
         select_path = current_dir
+        collected_files_dir: list[str] = []
         while True:
-            selected_path = select_file_or_dir(select_path, action)
+            selected_path = select_file_or_dir(select_path, action, collected_files_dir)
             if selected_path is None:
                 break
-            select_path = selected_path
+            select_path = os.path.join(select_path, selected_path)
             if os.path.isfile(select_path):
                 break
-            # selected path is the file that we want to work on
-            # TODO: Implement the logic to refactor, repair, test, or review the file here
+        # collected_files  we want to work on
+        # TODO: Implement the logic to refactor, repair, test, or review the files here
+        print(f"List of files : {collected_files_dir}")
+        radiolist_dialog(
+            title="Codeseus",
+            text="You have selected the following files and folders to work on"
+            + "\n".join(collected_files_dir),
+            values=[("Continue", "continue"), ("Cancel", "cancel")],
+            style=Style.from_dict(
+                {
+                    "dialog": "bg:#D6D1D1",
+                    "checkbox": "#DD31EB",
+                    "dialog.body": "bg:#F1EDED",
+                    "button": "bg:#E0CEBD",
+                    "frame.label": "#2881F2",
+                    "dialog.body label": "#31AAEB",
+                }
+            ),
+        ).run()
     else:
         print("No option selected.")
 
--- codeseus/cli/utils.py
+++ codeseus/cli/utils.py
@@ -3,21 +3,30 @@
 import os
 from typing import List, Optional, Tuple
 
-from prompt_toolkit.shortcuts import radiolist_dialog
+from prompt_toolkit.shortcuts import checkboxlist_dialog
 from prompt_toolkit.styles import Style
 
 
 def list_to_options(lst: List[str]) -> List[Tuple[str, str]]:
     """Convert the list of strings into a list of tuples for radiolist_dialog Options."""
-    return [(item, item) for item in lst]
+    return (
+        [("Select All", "*")]
+        + [("Back", "..")]
+        + [(item, item) for item in lst]
+        + [("Done", "Done")]
+    )
 
 
-def select_file_or_dir(select_path: str, action: str) -> Optional[str]:
+def select_file_or_dir(
+    select_path: str, action: str, collected_files_dir: List[str]
+) -> Optional[str]:
     """Prompts the user to select a file or directory from the given path using a radiolist_dialog."""
     options = os.listdir(select_path)
-    select_file_or_dir = radiolist_dialog(
+    select_file_or_dir = checkboxlist_dialog(
         title="Codeseus",
-        text=f"Select a file or folder to {action} in {select_path}",
+        text="selected:"
+        + "".join(collected_files_dir)
+        + f"\n\n Select a file or folder to {action} in {select_path}",
         values=list_to_options(options),
         style=Style.from_dict(
             {
@@ -30,27 +39,32 @@ def select_file_or_dir(select_path: str, action: str) -> Optional[str]:
             }
         ),
     ).run()
-
     if select_file_or_dir is None:
         return None
-
-    select_file_or_dir = os.path.join(select_path, select_file_or_dir)
-    if os.path.isdir(select_file_or_dir):
-        return select_file_or_dir
+    elif len(select_file_or_dir) == 1:
+        if select_file_or_dir[0] == "Back":
+            return os.path.dirname(select_path)
+        elif select_file_or_dir[0] == "Select All":
+            extra_options = ["Back", "Select All", "Done"]
+            for file_or_dir in options:
+                if file_or_dir not in extra_options:
+                    inner_collected = os.path.join(select_path, file_or_dir)
+                    collected_files_dir.append(inner_collected)
+            return select_path
+        elif select_file_or_dir[0] == "Done":
+            return None
+        else:
+            if os.path.isdir(select_file_or_dir[0]):
+                return select_file_or_dir[0]
+            else:
+                collected_files_dir.append(
+                    os.path.join(select_path, select_file_or_dir[0])
+                )
+                return os.path.dirname(select_path)
     else:
-        radiolist_dialog(
-            title="Codeseus",
-            text=f"Are you sure you want to {action} {select_file_or_dir}",
-            values=list_to_options(["Yes", "No"]),
-            style=Style.from_dict(
-                {
-                    "dialog": "bg:#D6D1D1",
-                    "checkbox": "#DD31EB",
-                    "dialog.body": "bg:#F1EDED",
-                    "button": "bg:#E0CEBD",
-                    "frame.label": "#2881F2",
-                    "dialog.body label": "#31AAEB",
-                }
-            ),
-        ).run()
-        return select_file_or_dir
+        extra_options = ["Back", "Select All", "Done"]
+        for file_or_dir in select_file_or_dir:
+            if file_or_dir not in extra_options:
+                inner_collected = os.path.join(select_path, file_or_dir)
+                collected_files_dir.append(inner_collected)
+        return os.path.dirname(select_path)
